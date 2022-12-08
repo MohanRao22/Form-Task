defc49b51bb7b254cd769fe6e5a96a1e8880c1f3
mohanrao <mohaneduplatform@gmail.com>
2022-12-08 23:24:07 +0530

    form control

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let firstname = document.querySelector("#firstname");
+let secondname = document.querySelector("#secondname");
+let nation = document.querySelector("#Nationality");
+let native = document.querySelector("#live");
+let email = document.querySelector("#email");
+let phone = document.querySelector("#phone");
